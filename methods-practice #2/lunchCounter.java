c0f2a77a6bc6f0f08a59ca8aeadcbb85871e47cf
Michael Kielbasinski <michael@kielbasinski.org>
2023-11-27 21:34:50 +0000

    random shit

--- methods-practice #2/lunchCounter.java
+++ methods-practice #2/lunchCounter.java
@@ -1,4 +1,3 @@
-
 /*
 Michael Kielbasinski
 
@@ -8,7 +7,7 @@ on the quantity entered and the predefined prices for each item. The program use
 loop to iterate through the lunch items and prompts the user for input. If the input is a valid number,
 it updates the total price accordingly. If the input is not a valid number, it displays an error message
 and prompts the user again.
- */
+*/
 import java.util.Scanner;
 import java.text.NumberFormat;
 
@@ -16,16 +15,14 @@ public class lunchCounter {
     private static Scanner scanner = new Scanner(System.in);
     private static NumberFormat formatter = NumberFormat.getCurrencyInstance();
     private static String temp;
-    private static double totalPrice = 0;
-    private static double[] prices = { 5.4, 6.5, 7.5 };
-
+    private static double[] count = {0,0,0};
     public static void main(String[] args) {
         for (int i = 0; i <= 2; i++) {
             while (true) {
                 System.out.println("How many units of LunchItem" + (i + 1) + " would you like?");
                 temp = scanner.nextLine();
                 if (temp.matches("\\d+")) {
-                    totalPrice += Integer.parseInt(temp) * prices[i];
+                    count[i]=Integer.parseInt(temp);
                     break;
                 } else {
                     System.out.println("Please enter a valid number");
@@ -33,6 +30,9 @@ public class lunchCounter {
                 }
             }
         }
-        System.out.println("Your total is " + formatter.format(totalPrice));
+        System.out.println("Your total is " + getTotal(count[0], count[1], count[2]));
+    }
+    public static String getTotal(double count1, double count2, double count3) {
+        return formatter.format(count1 * 5.4 + count2 * 6.5 + count3 * 7.5);
     }
 }
